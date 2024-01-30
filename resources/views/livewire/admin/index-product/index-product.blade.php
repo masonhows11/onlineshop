f3b24d4066509ef6e606c2027702f89d3315ad7e
mason.hows11 <mason.hows11@gmail.com>
2024-01-30 19:36:19 +0330

    index product style in responsive updated

--- resources/views/livewire/admin/index-product/index-product.blade.php
+++ resources/views/livewire/admin/index-product/index-product.blade.php
@@ -9,7 +9,8 @@
 
         <div class="row bg-white rounded rounded-2  add-product-section">
             <div class="col-lg-11 col-md-2 col-sm-2 py-4">
-                <a href="{{ route('admin.product.create.basic') }}" class="btn btn-primary">{{ __('messages.new_product') }}</a>
+                <a href="{{ route('admin.product.create.basic') }}"
+                   class="btn btn-primary">{{ __('messages.new_product') }}</a>
             </div>
         </div>
 
@@ -21,137 +22,143 @@
 
         <div class="row bg-white">
 
-                   <div class="col-sm-3">
-                       <div class="mb-3">
-                           <label for="title_search" class="form-label">{{ __('messages.title') }}</label>
-                           <input type="text" class="form-control" wire:model.debounce.500ms="search" id="title_search" >
-                       </div>
-                   </div>
+            <div class="col-sm-3">
+                <div class="mb-3">
+                    <label for="title_search" class="form-label">{{ __('messages.title') }}</label>
+                    <input type="text" class="form-control" wire:model.debounce.500ms="search" id="title_search">
+                </div>
+            </div>
 
-                   <div class="col-sm-3">
-                       <div class="mb-3">
-                           <label for="orderBy_filter" class="form-label">{{ __('messages.orderBy') }}</label>
-                           <select class="form-control"  wire:model.debounce.500ms="orderBy" id="orderBy_filter">
-                               <option value="id">شناسه</option>
-                               <option value="title_persian">عنوان (فارسی)</option>
-                               <option value="title_english">عنوان (انگلیسی)</option>
-                               <option value="created_at">تاریخ ایجاد</option>
-                           </select>
-                       </div>
-                   </div>
+            <div class="col-sm-3">
+                <div class="mb-3">
+                    <label for="orderBy_filter" class="form-label">{{ __('messages.orderBy') }}</label>
+                    <select class="form-control" wire:model.debounce.500ms="orderBy" id="orderBy_filter">
+                        <option value="id">شناسه</option>
+                        <option value="title_persian">عنوان (فارسی)</option>
+                        <option value="title_english">عنوان (انگلیسی)</option>
+                        <option value="created_at">تاریخ ایجاد</option>
+                    </select>
+                </div>
+            </div>
 
-                   <div class="col-sm-3">
-                       <div class="mb-3">
-                           <label for="orderAsc_filter" class="form-label">{{ __('messages.orderBy') }}</label>
-                           <select class="form-control"  wire:model.debounce.500ms="orderAsc" id="orderAsc_filter">
-                               <option>{{ __('messages.choose') }}</option>
-                               <option value="ASC">صعودی</option>
-                               <option value="DESC">نزولی</option>
-                           </select>
-                       </div>
-                   </div>
+            <div class="col-sm-3">
+                <div class="mb-3">
+                    <label for="orderAsc_filter" class="form-label">{{ __('messages.orderBy') }}</label>
+                    <select class="form-control" wire:model.debounce.500ms="orderAsc" id="orderAsc_filter">
+                        <option>{{ __('messages.choose') }}</option>
+                        <option value="ASC">صعودی</option>
+                        <option value="DESC">نزولی</option>
+                    </select>
+                </div>
+            </div>
 
-                   <div class="col-sm-3">
-                       <div class="mb-3">
-                           <label for="paginate_filter" class="form-label">{{ __('messages.paginate') }}</label>
-                           <select class="form-control" wire:model.debounce.500ms="paginate" id="paginate_filter">
-                               <option value="5">5</option>
-                               <option value="10">10</option>
-                               <option value="15">15</option>
-                               <option value="20">20</option>
-                           </select>
-                       </div>
-                   </div>
+            <div class="col-sm-3">
+                <div class="mb-3">
+                    <label for="paginate_filter" class="form-label">{{ __('messages.paginate') }}</label>
+                    <select class="form-control" wire:model.debounce.500ms="paginate" id="paginate_filter">
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+                </div>
+            </div>
         </div>
 
 
         <div class="row mt-5 result-search-product list-products">
-            <div class="col">
-                <table class="table table-bordered border-2 rounded-3 bg-white">
-                    <thead>
+
+            <table class="table py-4 table-bordered border-2 rounded-3 bg-white">
+                <thead class="">
+                <tr class="text-center">
+                    <th class="pro-field p-4">{{ __('messages.id') }}</th>
+
+                    <th class="p-4">{{ __('messages.image') }}</th>
+                    <th class="p-4">{{ __('messages.name_persian') }}</th>
+
+                    <th class="pro-field p-4">{{ __('messages.status') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_price') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_guarantee') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_meta') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_images') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_colors') }}</th>
+                    <th class="pro-field p-4">{{ __('messages.product_tags') }}</th>
+
+                    <th class="p-4">{{ __('messages.edit_model') }}</th>
+                    <th class="p-4">{{ __('messages.delete_model')}}</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach( $products as $product)
                     <tr class="text-center">
-                        <th class="pro-field">{{ __('messages.id') }}</th>
+                        <td class="pro-field">{{ $product->id }}</td>
 
-                        <th>{{ __('messages.image') }}</th>
-                        <th>{{ __('messages.name_persian') }}</th>
+                        <td>
+                            @if( $product->thumbnail_image && \Illuminate\Support\Facades\Storage::disk('public')->exists($product->thumbnail_image ) )
+                                <img class="img-thumbnail" width="100" height="100"
+                                     src="{{ asset('storage/'.$product->thumbnail_image) }}" alt="product_image">
+                            @else
+                                <img class="img-thumbnail" width="100" height="100"
+                                     src="{{ asset('admin_assets/images/no-image-icon-23494.png') }}"
+                                     alt="product_image">
+                            @endif
+                        </td>
+                        <td>
+                            <div class="mt-3">{{ Str::limit($product->title_persian,50)  }}</div>
+                        </td>
 
-                        <th class="pro-field">{{ __('messages.status') }}</th>
-                        <th class="pro-field">{{ __('messages.product_price') }}</th>
-                        <th class="pro-field">{{ __('messages.product_guarantee') }}</th>
-                        <th class="pro-field">{{ __('messages.product_meta') }}</th>
-                        <th class="pro-field">{{ __('messages.product_images') }}</th>
-                        <th class="pro-field">{{ __('messages.product_colors') }}</th>
-                        <th class="pro-field">{{ __('messages.product_tags') }}</th>
+                        <td class="pro-field">
+                            <a href="javascript:void(0)" wire:click.prevent="changeState({{ $product->id }})"
+                               class="btn btn-sm  {{ $product->status == 1 ? 'btn-success': 'btn-danger' }}">
+                                {{ $product->status == 1 ? __('messages.published')  : __('messages.unpublished') }}
+                            </a>
+                        </td>
+                        <td class="pro-field">
+                            <p class="mt-2">
+                                {{ priceFormat($product->origin_price) }} {{__('messages.toman')}}
+                            </p>
+                        </td>
+                        <td class="pro-field">
+                            <a href="{{ route('admin.product.guarantee.index',['product' => $product->id ]) }}"><i
+                                    class="fa fa-shield-alt mt-3"></i></a>
+                        </td>
+                        <td class="pro-field">
+                            <a href="{{ route('admin.product.create.meta',['product' => $product->id ]) }}"><i
+                                    class="fa fa-list mt-3"></i></a>
+                        </td>
+                        <td class="pro-field">
+                            <a href="{{ route('admin.product.create.images',['product' => $product->id ]) }}"><i
+                                    class="fa fa-images mt-3"></i></a>
+                        </td>
+                        <td class="pro-field">
+                            <a href="{{ route('admin.product.create.colors',['product' => $product->id ]) }}"><i
+                                    class="fa fa-paint-brush mt-3"></i></a>
+                        </td>
+                        <td class="pro-field">
+                            <a href="{{ route('admin.product.create.tags',['product' => $product->id ]) }}"><i
+                                    class="fa fa-tags mt-3"></i></a>
+                        </td>
 
-                        <th>{{ __('messages.edit_model') }}</th>
-                        <th>{{ __('messages.delete_model')}}</th>
+                        <td>
+                            <a class="mt-3" href="{{ route('admin.product.edit.basic',['product'=>$product->id]) }}"><i
+                                    class="mt-3 fa fa-edit"></i></a></td>
+                        <td>
+                            <a class="mt-3" href="javascript:void(0)"
+                               wire:click.prevent="deleteConfirmation({{ $product->id }})">
+                                <i class="mt-3 fa fa-trash"></i>
+                            </a>
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach( $products as $product)
-                        <tr class="text-center">
-                            <td class="">{{ $product->id }}</td>
+                @endforeach
+                </tbody>
+            </table>
 
-                            <td>
-                                @if( $product->thumbnail_image && \Illuminate\Support\Facades\Storage::disk('public')->exists($product->thumbnail_image ) )
-                                <img class="img-thumbnail" width="100" height="100" src="{{ asset('storage/'.$product->thumbnail_image) }}" alt="product_image">
-                                @else
-                                    <img class="img-thumbnail" width="100" height="100" src="{{ asset('admin_assets/images/no-image-icon-23494.png') }}" alt="product_image">
-                                @endif
-                            </td>
-                            <td>
-                                <div class="mt-3">{{ Str::limit($product->title_persian,50)  }}</div>
-                            </td>
-
-                            <td class="">
-                                <a href="javascript:void(0)" wire:click.prevent="changeState({{ $product->id }})"
-                                   class="btn btn-sm  {{ $product->status == 1 ? 'btn-success': 'btn-danger' }}">
-                                    {{ $product->status == 1 ? __('messages.published')  : __('messages.unpublished') }}
-                                </a>
-                            </td>
-                            <td class="">
-                               <p class="mt-2">
-                                   {{ priceFormat($product->origin_price) }} {{__('messages.toman')}}
-                               </p>
-                            </td>
-                            <td class="">
-                                <a href="{{ route('admin.product.guarantee.index',['product' => $product->id ]) }}"><i class="fa fa-shield-alt mt-3"></i></a>
-                            </td>
-                            <td class="">
-                                <a href="{{ route('admin.product.create.meta',['product' => $product->id ]) }}"><i
-                                        class="fa fa-list mt-3"></i></a>
-                            </td>
-                            <td class="">
-                                <a href="{{ route('admin.product.create.images',['product' => $product->id ]) }}"><i
-                                        class="fa fa-images mt-3"></i></a>
-                            </td>
-                            <td class="">
-                                <a href="{{ route('admin.product.create.colors',['product' => $product->id ]) }}"><i
-                                        class="fa fa-paint-brush mt-3"></i></a>
-                            </td>
-                            <td class="">
-                                <a href="{{ route('admin.product.create.tags',['product' => $product->id ]) }}"><i
-                                        class="fa fa-tags mt-3"></i></a>
-                            </td>
-
-                            <td>
-                                <a class="mt-3" href="{{ route('admin.product.edit.basic',['product'=>$product->id]) }}"><i class="mt-3 fa fa-edit"></i></a></td>
-                            <td>
-                                <a class="mt-3" href="javascript:void(0)" wire:click.prevent="deleteConfirmation({{ $product->id }})">
-                                    <i class="mt-3 fa fa-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
         </div>
 
         <div class="row d-flex justify-content-center">
-           <div class="col-lg-2">
-               {{ $products->links() }}
-           </div>
+            <div class="col-lg-2">
+                {{ $products->links() }}
+            </div>
         </div>
 
     </div>
