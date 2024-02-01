ee4ebed1148927fcb026ae3873f0cff2bceec8bb
mason.hows11 <mason.hows11@gmail.com>
2024-02-01 06:07:06 +0330

    uncomment sitemap routes

--- routes/web.php
+++ routes/web.php
@@ -89,12 +89,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 
-//Route::get('/sitemap.xml',[\App\Http\Controllers\SiteMapController::class,'index'])->name('sitemap.xml');
-//Route::get('/sitemap.xml/products',[\App\Http\Controllers\SiteMapController::class,'products'])->name('sitemap.xml.products');
-//Route::get('/sitemap.xml/categories',[\App\Http\Controllers\SiteMapController::class,'categories'])->name('sitemap.xml.categories');
-//Route::get('/sitemap.xml/tags',[\App\Http\Controllers\SiteMapController::class,'tags'])->name('sitemap.xml.tags');
-//Route::get('/sitemap.xml/static',[\App\Http\Controllers\SiteMapController::class,'static'])->name('sitemap.xml.static');
-//
+Route::get('/sitemap.xml',[\App\Http\Controllers\SiteMapController::class,'index'])->name('sitemap.xml');
+Route::get('/sitemap.xml/products',[\App\Http\Controllers\SiteMapController::class,'products'])->name('sitemap.xml.products');
+Route::get('/sitemap.xml/categories',[\App\Http\Controllers\SiteMapController::class,'categories'])->name('sitemap.xml.categories');
+Route::get('/sitemap.xml/tags',[\App\Http\Controllers\SiteMapController::class,'tags'])->name('sitemap.xml.tags');
+Route::get('/sitemap.xml/static',[\App\Http\Controllers\SiteMapController::class,'static'])->name('sitemap.xml.static');
+
 //Route::get('/storage-link', function () {
 //    symlink(storage_path('app/public'), $_SERVER['DOCUMENT_ROOT'] . '/storage');
 //});
