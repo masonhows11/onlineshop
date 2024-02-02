4fc602c3355c6b8ff43044931e757218198d10e1
mason.hows11 <mason.hows11@gmail.com>
2024-02-02 18:58:16 +0330

    create attribute updated

--- app/Http/Livewire/Admin/Attribute/AdminAttributeCreate.php
+++ app/Http/Livewire/Admin/Attribute/AdminAttributeCreate.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Livewire\Admin\Attribute;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class AdminAttributeCreate extends Component
 {
     public $category;
+    public $category_id;
 
     public function mount($category){
 
+        $this->category_id = $category;
     }
     public function render()
     {
         return view('livewire.admin.attribute.admin-attribute-create')
             ->extends('admin_end.include.master_dash')
             ->section('dash_main_content')
-            ->with(['' => ]);
+            ->with(['attributes' => DB::table('attributes')
+                ->where('category_id',$this->category_id)->get() ]);
     }
 }
--- resources/views/livewire/admin/attrbute/admin-attribute-create.blade.php
+++ resources/views/livewire/admin/attrbute/admin-attribute-create.blade.php
@@ -22,19 +22,27 @@
                     <tr class="text-center">
                         <th>{{ __('messages.id') }} </th>
                         <th>{{ __('messages.name')}}</th>
-                        <th>{{ __('messages.operation')}}</th>
+                        <th>{{ __('messages.type') }}</th>
+                        <th>{{ __('messages.add_new_value') }}</th>
+                        <th>{{ __('messages.has_default_value') }}</th>
+                        <th>{{ __('messages.edit_model')}}</th>
+                        <th>{{ __('messages.delete_model')}}</th>
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach($categories as $category)
+                    @foreach($attributes as $attribute)
                         <tr class="text-center">
-                            <td>{{ $category->id }}</td>
-                            <td>{{ $category->title_persian }}</td>
+                            <td>{{ $attribute->id }}</td>
+                            <td>{{ $attribute->name }}</td>
+                            <td>{{ $attribute->has_default_value }}</td>
                             <td>
-                                <a href="{{ route('admin.attribute.create',['category'=>$category->id]) }}" class="mx-4 btn btn-sm btn-success ">
-                                    {{ __('messages.add_new_specification') }}
+                                <a href="" class="mx-4 btn btn-sm btn-success ">
+
                                 </a>
                             </td>
+                            <td></td>
+                            <td></td>
+
                         </tr>
                     @endforeach
                     </tbody>
@@ -51,3 +59,53 @@
     </div>
 
 </div>
+@push('dash_custom_script')
+    <script type="text/javascript">
+        window.addEventListener('show-delete-confirmation', event => {
+            Swal.fire({
+                title: 'آیا مطمئن هستید این ایتم حذف شود؟',
+                icon: 'error',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'بله حذف کن!',
+                cancelButtonText: 'خیر',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emit('deleteConfirmed')
+                }
+            });
+        })
+    </script>
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            showCloseButton: true,
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        });
+        window.addEventListener('show-result', ({detail: {type, message}}) => {
+            Toast.fire({
+                icon: type,
+                title: message
+            })
+        })
+        @if(session()->has('warning'))
+        Toast.fire({
+            icon: 'warning',
+            title: '{{ session()->get('warning') }}'
+        })
+        @elseif(session()->has('success'))
+        Toast.fire({
+            icon: 'success',
+            title: '{{ session()->get('success') }}'
+        })
+        @endif
+    </script>
+@endpush
