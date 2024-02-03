d0b15a8ee919baaab896ffdf7a84eb548ce137c9
mason.hows11 <mason.hows11@gmail.com>
2024-02-03 22:58:45 +0330

    ready for attach attribute to product using attribute product model

--- app/Models/AttributeProduct.php
+++ app/Models/AttributeProduct.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class AttributeProduct extends Model
 {
     use HasFactory;
+    protected $table = 'attribute_products';
+    protected $fillable = [];
+
 }
